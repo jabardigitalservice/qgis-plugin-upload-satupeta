1910ef6815f9176d0d61fe0bc54643235f429526
Misdan Wijaya <misdan.wijaya07@gmail.com>
2022-11-21 16:16:19 +0700

    update latest

---
+++ workspace/utils/worker.py
@@ -0,0 +1,88 @@
+import json
+import os
+import requests
+
+from zipfile import ZipFile
+from PyQt5.QtCore import QThread, pyqtSignal
+
+from ..utils.config import readSetting
+
+
+class WorkerThread(QThread):
+
+    worker_complete = pyqtSignal(dict)
+
+    def __init__(self, source,tipeFile):
+        super(QThread, self).__init__()
+        #print('workerinit')
+        self.stopworker = False # initialize the stop variable
+
+        self.source = source
+        self.tipeFile = tipeFile
+
+    def run(self):
+
+        shp = self.source.replace(self.tipeFile, ".shp")
+        shp = shp.replace("\\", "/")
+        prj = self.source.replace(self.tipeFile, ".prj")
+        prj = prj.replace("\\", "/")
+        dbf = self.source.replace(self.tipeFile, ".dbf")
+        dbf = dbf.replace("\\", "/")
+        shx = self.source.replace(self.tipeFile, ".shx")
+        shx = shx.replace("\\", "/")
+
+        #for check if necessary
+        #sourceFile = json.loads('{"shp":"%s","prj":"%s","dbf":"%s","shx":"%s"}'%(shp,prj,dbf,shx))
+
+        zipShp = ZipFile(f"{shp.split('.')[0]}"+'.zip', 'w')
+        # Add multiple files to the zip
+        zipShp.write(f"{shp}",os.path.basename(shp).replace(" ","_"))
+        zipShp.write(f"{dbf}",os.path.basename(dbf).replace(" ","_"))
+        zipShp.write(f"{shx}",os.path.basename(shx).replace(" ","_"))
+        zipShp.write(f"{prj}",os.path.basename(prj).replace(" ","_"))
+        # close the Zip File
+        zipShp.close()
+
+        #path zip
+        files_path = shp.split('.')[0]+'.zip'
+
+        #read store setting
+        self.username = readSetting("username")
+        self.password = readSetting("password")
+        self.opdcode = readSetting("opdcode")
+
+        headers = {
+            'user': self.username,
+            'password': self.password,
+            'opdcode': self.opdcode
+        }
+
+        #another endpoint upload_v2
+        url = 'http://geoplugin.coredatajds.id/gis/plugin/upload/'
+
+        if files_path:
+
+            #name file
+            file_name = shp.split('.')[0].split('/')[-1]
+            file_extension = ".zip"
+
+            #sent file
+            response = requests.post(url, headers=headers,files=[
+                ('file',(file_name+file_extension,
+                open(files_path,'rb'),
+                'application/zip'))
+            ],verify=False)
+
+            if response:
+                if response.status_code == 200:
+                    self.worker_complete.emit({"status":response.status_code,"message":"OK"})
+                else:
+                    err_msg = response.json().get('detail').get('message')
+                    self.worker_complete.emit({"status":response.status_code,"message":err_msg})
+            else:
+                self.worker_complete.emit({"status":417,"message":"Failed Upload"})
+        else:
+            self.worker_complete.emit({"status":416,"message":"Empty File"})
+
+        #delete file tmp
+        os.remove(files_path)
